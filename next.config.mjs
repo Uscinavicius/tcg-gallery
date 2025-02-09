60fe826318ccb100f12d3806a4fe0897ee09f8d3
uscinavicius <uscinaviciusd@gmail.com>
2025-02-09 13:19:20 -0600

    Add dotenv dependency and configure Next.js settings; update Home component to display Pokémon cards

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['images.pokemontcg.io'],
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+        config.resolve.fallback.fs = false;
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
