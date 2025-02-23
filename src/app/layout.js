5cfe2380b6907fa51176d7e9a63ae37954b7582c
uscinavicius <uscinaviciusd@gmail.com>
2025-02-22 19:07:44 -0600

    Add @vercel/analytics dependency and integrate analytics component in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>
         <Nav />
         <div className="pt-16">
