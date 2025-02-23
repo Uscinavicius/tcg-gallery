d50dd6285fe8442d57644cf476a1cd976f6b5c13
uscinavicius <uscinaviciusd@gmail.com>
2025-02-22 19:08:40 -0600

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Analytics/>
+      <SpeedInsights/>
       <body className={inter.className}>
         <Nav />
         <div className="pt-16">
